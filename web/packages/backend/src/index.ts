9eba166de6fec6f3941db6b7b0368c6ec78abc31
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-02-11 10:36:17 +0900

    fix to store data

--- web/packages/backend/src/index.ts
+++ web/packages/backend/src/index.ts
@@ -6,7 +6,9 @@ import {
   USE_HTTPS,
 } from '@backend/env'
 import { app } from '@getcronit/pylon'
+import { randomUUIDv7 } from 'bun'
 import { cors } from 'hono/cors'
+import { HTTPException } from 'hono/http-exception'
 import { secureHeaders } from 'hono/secure-headers'
 import { z } from 'zod'
 
@@ -20,49 +22,60 @@ const ticketSchema = z.object({
 })
 type Ticket = z.infer<typeof ticketSchema>
 
+const store: {
+  tickets: Ticket[]
+} = {
+  tickets: [
+    {
+      ticketId: 'xxxx-001',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      title: 'Some Ticket 1',
+      description: 'Some ticket description here.',
+      deadline: new Date(),
+    },
+    {
+      ticketId: 'xxxx-002',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+      title: 'Some Ticket 2',
+    },
+  ],
+}
+
 export const graphql = {
   Query: {
     hello: () => {
       return 'Hello, world!'
     },
-    tickets: async (): Promise<Ticket[]> => [
-      {
-        ticketId: 'xxxx-001',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        title: 'Some Ticket 1',
-        description: 'Some ticket description here.',
-        deadline: new Date(),
-      },
-      {
-        ticketId: 'xxxx-002',
-        createdAt: new Date(),
-        updatedAt: new Date(),
-        title: 'Some Ticket 2',
-      },
-    ],
-    ticket: async (id: string): Promise<Ticket> => ({
-      ticketId: 'xxxx',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      title: 'Some Ticket',
-      description: 'Some ticket description here.',
-      deadline: new Date(),
-    }),
+    tickets: async () => {
+      return store.tickets
+    },
+    ticket: async (id: string) => {
+      const ret = store.tickets.find((t) => t.ticketId === id)
+      if (!ret) {
+        throw new HTTPException(404, { message: 'not found' })
+      }
+      return ret
+    },
   },
   Mutation: {
     createTicket: async (
       title: string,
       description?: string,
       deadline?: Date,
-    ): Promise<Ticket> => ({
-      ticketId: 'xxxx',
-      createdAt: new Date(),
-      updatedAt: new Date(),
-      title,
-      description,
-      deadline,
-    }),
+    ) => {
+      const newTicket = {
+        ticketId: randomUUIDv7(),
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        title,
+        description,
+        deadline,
+      }
+      store.tickets.push(newTicket)
+      return newTicket
+    },
   },
 }
 
