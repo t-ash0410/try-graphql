51db822be50245cf3b34f61edd235776fab20565
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-02-14 19:21:31 +0900

    enable https

--- web/packages/frontend/vite.config.ts
+++ web/packages/frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import { readFileSync } from 'node:fs'
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
@@ -5,4 +6,11 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  server: {
+    https: {
+      key: readFileSync(process.env.TLS_KEY_PATH || ''),
+      cert: readFileSync(process.env.TLS_CERT_PATH || ''),
+    },
+    proxy: {},
+  },
 })
