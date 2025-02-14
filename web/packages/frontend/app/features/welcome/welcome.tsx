cb2131370d113ff70e41c23dcbeab0c01c76bb84
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-02-14 21:12:48 +0900

    remove welcome page

--- web/packages/frontend/app/features/welcome/welcome.tsx
+++
@@ -1,5 +0,0 @@
-export function Welcome() {
-  return (
-    <main className="flex items-center justify-center pt-16 pb-4">Hello</main>
-  )
-}
--- web/packages/frontend/app/routes.ts
+++ web/packages/frontend/app/routes.ts
@@ -1,8 +1,6 @@
-import { type RouteConfig, index, route } from '@react-router/dev/routes'
+import { type RouteConfig, route } from '@react-router/dev/routes'
 import { pagePaths } from './consts'
 
 export default [
-  index('routes/home.tsx'),
-
   route(pagePaths.signin.path, pagePaths.signin.file),
 ] satisfies RouteConfig
