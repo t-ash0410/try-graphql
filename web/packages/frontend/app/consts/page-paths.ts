daa2ad88b3e60e3775f84b3ecb7dbc3034701836
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-02-14 17:25:15 +0900

    add consts dir

---
+++ web/packages/frontend/app/consts/page-paths.ts
@@ -0,0 +1,19 @@
+export const pagePaths = {
+  root: {
+    path: '/',
+  },
+  signin: {
+    path: '/signin',
+    title: 'サインイン',
+  },
+  sso: {
+    slack: {
+      path: '/sso/slack',
+      title: 'Redirect',
+    },
+  },
+  tickets: {
+    path: '/tickets',
+    title: 'チケット一覧',
+  },
+}
