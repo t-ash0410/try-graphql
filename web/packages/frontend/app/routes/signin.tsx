e3bb72d0e319b5d8dae47f653d46955a4a76d586
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-02-14 17:27:22 +0900

    add signin route

---
+++ web/packages/frontend/app/routes/signin.tsx
@@ -0,0 +1,13 @@
+import { SigninForm } from '~/features/signin'
+import type { Route } from './+types/signin'
+
+export function meta(_: Route.MetaArgs) {
+  return [
+    { title: 'Sign in' },
+    { name: 'description', content: 'Welcome to Try GraphQL!' },
+  ]
+}
+
+export default function Home() {
+  return <SigninForm />
+}
