38ba48870dca2afcea2d693811be3230c04399d1
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-02-14 22:21:40 +0900

    impl slack sso route

--- web/packages/frontend/app/routes.ts
+++ web/packages/frontend/app/routes.ts
@@ -4,4 +4,5 @@ import { pagePaths } from './consts'
 export default [
   index(pagePaths.root.file),
   route(pagePaths.signin.path, pagePaths.signin.file),
+  route(pagePaths.ssoSlack.path, pagePaths.ssoSlack.file),
 ] satisfies RouteConfig
