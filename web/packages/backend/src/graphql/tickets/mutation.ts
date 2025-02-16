37c5fe861b9eb6157c41db86fb4707439c93805e
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-02-17 03:32:08 +0900

    impl mutations

--- web/packages/backend/src/graphql/tickets/mutation.ts
+++ web/packages/backend/src/graphql/tickets/mutation.ts
@@ -1,7 +1,7 @@
 import { db } from '@backend/lib/db'
 import { ServiceError, createDecorator, getContext } from '@getcronit/pylon'
 
-const validate = createDecorator(
+const validateCreate = createDecorator(
   async (title: string, _: string, deadline?: string) => {
     if (title.length < 1) {
       throw new ServiceError('Title cannot be empty', {
@@ -18,7 +18,7 @@ const validate = createDecorator(
   },
 )
 
-const createTicket = validate(
+const createTicket = validateCreate(
   async (title: string, description?: string, deadline?: string) => {
     const ctx = getContext()
 
@@ -41,4 +41,80 @@ const createTicket = validate(
   },
 )
 
-export { createTicket }
+const validateUpdateTicketTitle = createDecorator(
+  async (_: number, title: string) => {
+    if (title.length < 1) {
+      throw new ServiceError('Title cannot be empty', {
+        code: 'INVALID_TITLE',
+        statusCode: 400,
+      })
+    }
+  },
+)
+
+const updateTicketTitle = validateUpdateTicketTitle(
+  async (ticketId: number, title: string) => {
+    await db.ticket.update({
+      data: {
+        title,
+      },
+      where: {
+        ticketId,
+      },
+    })
+  },
+)
+
+const updateTicketDescription = async (
+  ticketId: number,
+  description?: string,
+) => {
+  await db.ticket.update({
+    data: {
+      description,
+    },
+    where: {
+      ticketId,
+    },
+  })
+}
+
+const validateUpdateTicketDeadline = createDecorator(
+  async (_: number, deadline?: string) => {
+    if (deadline && Number.isNaN(new Date(deadline).getDate())) {
+      throw new ServiceError('Invalid deadline', {
+        code: 'INVALID_DEADLINE',
+        statusCode: 400,
+      })
+    }
+  },
+)
+
+const updateTicketDeadline = validateUpdateTicketDeadline(
+  async (ticketId: number, deadline?: string) => {
+    await db.ticket.update({
+      data: {
+        deadline,
+      },
+      where: {
+        ticketId,
+      },
+    })
+  },
+)
+
+const deleteTicket = async (ticketId: number) => {
+  await db.ticket.delete({
+    where: {
+      ticketId,
+    },
+  })
+}
+
+export {
+  createTicket,
+  updateTicketTitle,
+  updateTicketDescription,
+  updateTicketDeadline,
+  deleteTicket,
+}
