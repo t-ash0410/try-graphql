c0484d7a70117bf29923c02b37e1894dc83b4f21
t-ash0410 <t_ash0410@yahoo.co.jp>
2025-02-17 15:14:02 +0900

    add tests

--- web/packages/backend/tests/tickets/mutation.test.ts
+++ web/packages/backend/tests/tickets/mutation.test.ts
@@ -1,4 +1,4 @@
-import { describe, expect, it } from 'bun:test'
+import { beforeAll, describe, expect, it } from 'bun:test'
 import { dummyUser } from '@backend-tests/fixtures'
 import { JWT_KEY } from '@backend/lib/env'
 import { createJWT } from '@backend/lib/jwt'
@@ -26,7 +26,6 @@ describe('Mutation.createTicket', async () => {
                 description: "This is a new ticket",
                 deadline: "2025-02-14T00:00:00+09:00"
               ) {
-                ticketId
                 title
                 description
                 deadline
@@ -44,7 +43,6 @@ describe('Mutation.createTicket', async () => {
           "createTicket": {
             "deadline": "2025-02-13T15:00:00.000Z",
             "description": "This is a new ticket",
-            "ticketId": 3,
             "title": "New Ticket",
           },
         },
@@ -151,6 +149,311 @@ describe('Mutation.createTicket', async () => {
             ],
           },
         ],
+      }
+    `)
+  })
+})
+
+describe('Mutation.updateTicketTitle', async () => {
+  const jwt = await createJWT({
+    userId: dummyUser.userId,
+    now: new Date(),
+  })
+
+  let ticketId: number
+
+  beforeAll(async () => {
+    const ret = await prisma?.ticket.create({
+      data: {
+        title: 'Ticket1',
+        description: 'Some Description',
+        deadline: new Date('2025-02-14T00:00:00'),
+        authorId: dummyUser.userId,
+      },
+    })
+    ticketId = ret.ticketId
+  })
+
+  it('returns success', async () => {
+    const res = await app.fetch(
+      new Request('https://localhost/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Cookie: `${JWT_KEY}=${jwt}`,
+        },
+        body: JSON.stringify({
+          query: `
+            mutation {
+              updateTicketTitle(
+                ticketId: ${ticketId},
+                title: "Updated"
+              )
+            }
+          `,
+        }),
+      }),
+    )
+
+    expect(res.status).toBe(200)
+    expect(await res.json()).toMatchInlineSnapshot(`
+      {
+        "data": {
+          "updateTicketTitle": null,
+        },
+      }
+    `)
+  })
+
+  it('returns an error when title is empty', async () => {
+    const res = await app.fetch(
+      new Request('https://localhost/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Cookie: `${JWT_KEY}=${jwt}`,
+        },
+        body: JSON.stringify({
+          query: `
+            mutation {
+              updateTicketTitle(
+                ticketId: ${ticketId},
+                title: ""
+              )
+            }
+          `,
+        }),
+      }),
+    )
+
+    expect(res.status).toBe(200)
+    expect(await res.json()).toMatchInlineSnapshot(`
+      {
+        "data": {
+          "updateTicketTitle": null,
+        },
+        "errors": [
+          {
+            "extensions": {
+              "code": "INVALID_TITLE",
+              "statusCode": 400,
+            },
+            "locations": [
+              {
+                "column": 15,
+                "line": 3,
+              },
+            ],
+            "message": "Title cannot be empty",
+            "path": [
+              "updateTicketTitle",
+            ],
+          },
+        ],
+      }
+    `)
+  })
+})
+
+describe('Mutation.updateTicketDescription', async () => {
+  const jwt = await createJWT({
+    userId: dummyUser.userId,
+    now: new Date(),
+  })
+
+  let ticketId: number
+
+  beforeAll(async () => {
+    const ret = await prisma?.ticket.create({
+      data: {
+        title: 'Ticket1',
+        description: 'Some Description',
+        deadline: new Date('2025-02-14T00:00:00'),
+        authorId: dummyUser.userId,
+      },
+    })
+    ticketId = ret.ticketId
+  })
+
+  it('returns success', async () => {
+    const res = await app.fetch(
+      new Request('https://localhost/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Cookie: `${JWT_KEY}=${jwt}`,
+        },
+        body: JSON.stringify({
+          query: `
+            mutation {
+              updateTicketDescription(
+                ticketId: ${ticketId},
+                description: "Updated"
+              )
+            }
+          `,
+        }),
+      }),
+    )
+
+    expect(res.status).toBe(200)
+    expect(await res.json()).toMatchInlineSnapshot(`
+      {
+        "data": {
+          "updateTicketDescription": null,
+        },
+      }
+    `)
+  })
+})
+
+describe('Mutation.updateTicketDeadline', async () => {
+  const jwt = await createJWT({
+    userId: dummyUser.userId,
+    now: new Date(),
+  })
+
+  let ticketId: number
+
+  beforeAll(async () => {
+    const ret = await prisma?.ticket.create({
+      data: {
+        title: 'Ticket1',
+        description: 'Some Description',
+        deadline: new Date('2025-02-14T00:00:00'),
+        authorId: dummyUser.userId,
+      },
+    })
+    ticketId = ret.ticketId
+  })
+
+  it('returns success', async () => {
+    const res = await app.fetch(
+      new Request('https://localhost/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Cookie: `${JWT_KEY}=${jwt}`,
+        },
+        body: JSON.stringify({
+          query: `
+            mutation {
+              updateTicketDeadline(
+                ticketId: ${ticketId},
+                deadline: "${(new Date()).toISOString()}"
+              )
+            }
+          `,
+        }),
+      }),
+    )
+
+    expect(res.status).toBe(200)
+    expect(await res.json()).toMatchInlineSnapshot(`
+      {
+        "data": {
+          "updateTicketDeadline": null,
+        },
+      }
+    `)
+  })
+
+  it('returns an error when title is empty', async () => {
+    const res = await app.fetch(
+      new Request('https://localhost/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Cookie: `${JWT_KEY}=${jwt}`,
+        },
+        body: JSON.stringify({
+          query: `
+            mutation {
+              updateTicketDeadline(
+                ticketId: ${ticketId},
+                deadline: "invalid"
+              )
+            }
+          `,
+        }),
+      }),
+    )
+
+    expect(res.status).toBe(200)
+    expect(await res.json()).toMatchInlineSnapshot(`
+      {
+        "data": {
+          "updateTicketDeadline": null,
+        },
+        "errors": [
+          {
+            "extensions": {
+              "code": "INVALID_DEADLINE",
+              "statusCode": 400,
+            },
+            "locations": [
+              {
+                "column": 15,
+                "line": 3,
+              },
+            ],
+            "message": "Invalid deadline",
+            "path": [
+              "updateTicketDeadline",
+            ],
+          },
+        ],
+      }
+    `)
+  })
+})
+
+describe('Mutation.deleteTicket', async () => {
+  const jwt = await createJWT({
+    userId: dummyUser.userId,
+    now: new Date(),
+  })
+
+  let ticketId: number
+
+  beforeAll(async () => {
+    const ret = await prisma?.ticket.create({
+      data: {
+        title: 'Ticket1',
+        description: 'Some Description',
+        deadline: new Date('2025-02-14T00:00:00'),
+        authorId: dummyUser.userId,
+      },
+    })
+    ticketId = ret.ticketId
+  })
+
+  it('returns success', async () => {
+    const res = await app.fetch(
+      new Request('https://localhost/graphql', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Cookie: `${JWT_KEY}=${jwt}`,
+        },
+        body: JSON.stringify({
+          query: `
+            mutation {
+              deleteTicket(
+                ticketId: ${ticketId}
+              )
+            }
+          `,
+        }),
+      }),
+    )
+
+    expect(res.status).toBe(200)
+    expect(await res.json()).toMatchInlineSnapshot(`
+      {
+        "data": {
+          "deleteTicket": null,
+        },
       }
     `)
   })
